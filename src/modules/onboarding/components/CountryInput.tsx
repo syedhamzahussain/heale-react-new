935661c04aa2cbc95c184dbfa4e58f12ae0a084b
WaleedAtiq <waleedatiq4@gmail.com>
2024-03-22 00:09:25 +0500

    mobile input height matched

--- src/modules/onboarding/components/CountryInput.tsx
+++ src/modules/onboarding/components/CountryInput.tsx
@@ -6,14 +6,14 @@ import { Box } from '@chakra-ui/react';
 const CountryInput = () => {
   const [phone, setPhone] = useState('');
   return (
-    <Box sx={{ '.react-tel-input .form-control': { height: '2.5rem !important' ,width: '100%' , paddingLeft: '38px', borderColor: '#E3E3FA', borderRadius: '5px' }, '.react-tel-input .flag-dropdown' : {borderRight: '0', borderTopLeftRadius: '5px' ,borderColor: '#E3E3FA', borderBottomLeftRadius: '5px'} , '.react-tel-input .selected-flag' : { backgroundColor: "#fff", borderTopLeftRadius: '5px' , borderBottomLeftRadius: '5px'},'.react-tel-input .flag-dropdown .selected-flag .flag .arrow': {display: 'none'}  }}>
+    <Box sx={{ '.react-tel-input .form-control': { height: '2.75rem !important', width: '100%', paddingLeft: '38px', borderColor: '#E3E3FA', borderRadius: '5px' }, '.react-tel-input .flag-dropdown': { borderRight: '0', borderTopLeftRadius: '5px', borderColor: '#E3E3FA', borderBottomLeftRadius: '5px' }, '.react-tel-input .selected-flag': { backgroundColor: "#fff", borderTopLeftRadius: '5px', borderBottomLeftRadius: '5px' }, '.react-tel-input .flag-dropdown .selected-flag .flag .arrow': { display: 'none' } }}>
       <PhoneInput
         country={'us'}
         value={phone}
-        onChange={setPhone}     
-    />
+        onChange={setPhone}
+      />
     </Box>
- 
+
   )
 }
 
--- src/modules/onboarding/components/Wizard/Authentication.tsx
+++ src/modules/onboarding/components/Wizard/Authentication.tsx
@@ -1,7 +1,7 @@
-import { Box, Flex, FormControl,InputGroup,Stack,InputLeftElement, FormLabel, Grid, GridItem, Heading, Input, Link, List, ListItem, Select, Text } from '@chakra-ui/react'
+import { Box, FormControl, FormLabel, Heading, Input, Link, Text } from '@chakra-ui/react'
 import ButtonTheme from 'modules/shared/ButtonTheme'
 import { CheckIcon, FlyEnvelopeIcon, alertIcon, trustIcon } from 'modules/shared/Icons';
-import React, { useState } from "react";
+import React from "react";
 import MessageBox from '../MessageBox';
 import CountryInput from '../CountryInput';
 
@@ -13,13 +13,13 @@ const Authentication = () => {
             <MessageBox btnText='Resend Email' desc='The verification Link has Expired. Please Click "Resend Email". and click the link in your email within 10 mins' icon={alertIcon} title='Resend Email' />
             <MessageBox trust btnText='Trust' desc="We don't recognize this device. Should we trust it for your next login? If this is a public computer, we recommend clicking Don't Trust 'Resend Email'. and click the link in your email within 10 mins" icon={trustIcon} title='Trust This Device?' />
             <Box>
-                 <Heading as={"h4"} mt={4} mb={2} fontSize={"3xl"} color={"Primary.Navy"}>Verify Your Phone Number</Heading>
-                 <Text fontSize={"sm"} fontWeight={500} color={"Neutral.800"}>We'll Send you a code via sms</Text>
-                 <FormControl mt={4}>
-                        <FormLabel fontSize={"sm"} fontWeight={500} color={"Primary.Navy"}>Phone</FormLabel>
-                        <CountryInput/>
-                 </FormControl>
-                 <ButtonTheme btnText={"Continue"} chakraProps={{
+                <Heading as={"h4"} mt={4} mb={2} fontSize={"3xl"} color={"Primary.Navy"}>Verify Your Phone Number</Heading>
+                <Text fontSize={"sm"} fontWeight={500} color={"Neutral.800"}>We'll Send you a code via sms</Text>
+                <FormControl mt={4}>
+                    <FormLabel fontSize={"sm"} fontWeight={500} color={"Primary.Navy"}>Phone</FormLabel>
+                    <CountryInput />
+                </FormControl>
+                <ButtonTheme btnText={"Continue"} chakraProps={{
                     width: "full",
                     marginTop: "8",
                     marginBottom: "8",
@@ -29,42 +29,42 @@ const Authentication = () => {
                 <Text fontSize={"xs"} color={"Neutral.800"}>By clicking "Continue", I understand and I authorize HEALE network to send me a one-time use code via SMS (text message). Carrier fees may apply.</Text>
             </Box>
             <Box>
-                 <Heading as={"h4"} mt={4} mb={2} fontSize={"3xl"} color={"Primary.Navy"}>Enter Verification Code</Heading>
-                 <Text fontSize={"sm"} fontWeight={500} color={"Neutral.800"}>We just sent you a 6 digit code by text. Please enter it below.</Text>
-                 <FormControl mt={4}>
-                        <FormLabel fontSize={"sm"} fontWeight={500} color={"Primary.Navy"}>Enter code</FormLabel>
-                        <Input type='number' mb={5} placeholder='6 digits' />
-                 </FormControl>
-                 <ButtonTheme btnText={"Submit"} chakraProps={{
+                <Heading as={"h4"} mt={4} mb={2} fontSize={"3xl"} color={"Primary.Navy"}>Enter Verification Code</Heading>
+                <Text fontSize={"sm"} fontWeight={500} color={"Neutral.800"}>We just sent you a 6 digit code by text. Please enter it below.</Text>
+                <FormControl mt={4}>
+                    <FormLabel fontSize={"sm"} fontWeight={500} color={"Primary.Navy"}>Enter code</FormLabel>
+                    <Input type='number' mb={5} placeholder='6 digits' />
+                </FormControl>
+                <ButtonTheme btnText={"Submit"} chakraProps={{
                     width: "full",
                 }} primary />
-                 <ButtonTheme btnText={"Resent Code"} chakraProps={{
+                <ButtonTheme btnText={"Resent Code"} chakraProps={{
                     width: "full",
                     marginTop: '10px',
                     marginBottom: '10px',
-                 }} primaryOutline/>
+                }} primaryOutline />
                 <Box textAlign="center" >
-                     <Link href="#" color='Primary.Blue' display={'inline-block'} >Use another phone number</Link>
+                    <Link href="#" color='Primary.Blue' display={'inline-block'} >Use another phone number</Link>
                 </Box>
                 <Text fontSize={"xs"} my={10} color={"Neutral.800"}>By clicking "Continue", I understand and I authorize HEALE network to send me a one-time use code via SMS (text message). Carrier fees may apply.</Text>
             </Box>
             <Box>
-                 <Heading as={"h4"} mt={4} mb={2} fontSize={"3xl"} color={"Primary.Navy"}>Enter Verification Code</Heading>
-                 <Text fontSize={"sm"} fontWeight={500} color={"Neutral.800"}>Content draft team underline variant plugin editor. Arrange slice reesizing library underline team ipsum figjam.</Text>
-                 <FormControl mt={4} >
-                        <FormLabel fontSize={"sm"} fontWeight={500} color={"Primary.Navy"}>Enter code</FormLabel>
-                        <Input type='number' mb={5} placeholder='6 digits' />
-                 </FormControl>
-                 <ButtonTheme btnText={"Submit"} chakraProps={{
+                <Heading as={"h4"} mt={4} mb={2} fontSize={"3xl"} color={"Primary.Navy"}>Enter Verification Code</Heading>
+                <Text fontSize={"sm"} fontWeight={500} color={"Neutral.800"}>Content draft team underline variant plugin editor. Arrange slice reesizing library underline team ipsum figjam.</Text>
+                <FormControl mt={4} >
+                    <FormLabel fontSize={"sm"} fontWeight={500} color={"Primary.Navy"}>Enter code</FormLabel>
+                    <Input type='number' mb={5} placeholder='6 digits' />
+                </FormControl>
+                <ButtonTheme btnText={"Submit"} chakraProps={{
                     width: "full",
                 }} primary />
-                 <ButtonTheme btnText={"Resent Code"} chakraProps={{
+                <ButtonTheme btnText={"Resent Code"} chakraProps={{
                     width: "full",
                     marginTop: '10px',
                     marginBottom: '10px',
-                 }} primaryOutline/>
+                }} primaryOutline />
                 <Box textAlign="center" >
-                     <Link href="#" color='Primary.Blue' display={'inline-block'} >Use another phone number</Link>
+                    <Link href="#" color='Primary.Blue' display={'inline-block'} >Use another phone number</Link>
                 </Box>
                 <Text fontSize={"xs"} my={10} color={"Neutral.800"}>By clicking this I understand I authorize the HEALE Network to text me fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nulla vitae elit libero, a pharetra augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis risus eget urna mollis ornare vel eu leo.</Text>
             </Box>
